35871bd9f8198c15c69724d2aa928438d4560fe1
unknown <raquelpm23@gmail.com>
2015-03-20 17:14:06 +0100

    starting advancesprings

--- pract2/Exercise.cpp
+++ pract2/Exercise.cpp
@@ -12,8 +12,60 @@ static const float k = 100.0f;
 // damping
 static const float d = 0.01f;
 
+//axis with distance to node
+Vector3 axis(-0.5, 0, 0);
+
+//Similar a Spring2D from practice 1
+class Spring{
+	
+private:
+	const Vector3 p1;
+	const Vector3 p2;
+	Vector3 f;
+	const float k;
+
+public:
+	Spring(const Vector3 p1, const Vector3 p2, const float k) :p1(p1), p2(p2), k(k){ f = -k*(p1 - p2); }
+	~Spring(void){}
+	const Vector3 getF1(void) const { return f; }
+	Vector3 getF2(void) const { return -f; }
+};
+
+
+void advanceConstraints(vector<RigidBody>& bodies, float step, bool collisions, float floor){
+
+}
+
+void advanceSprings(vector<RigidBody>& bodies, float step, bool collisions, float floor){
+	int nbodies = bodies.size();
+
+	//Initialize forces with gravity and damping
+	vector<Vector3> F(nbodies), T(nbodies);
+	for (int i = 0; i < nbodies; i++){
+		F[i] = Vector3(0.0f, -bodies[i].Mass()*g, 0.0f);
+		T[i] = -d*bodies[i].Omega();
+	}
+
+	//Add springs forces
+	//Create first spring with position 0 as p1
+	Spring sp0 (Vector3::ZERO, bodies[0].posLocalToGlobal(axis), k);
+	F[0] += sp0.getF2();
+	T[0] += Vector3::crossProd(bodies[0].vecLocalToGlobal(axis),sp0.getF2());
+	//Add spring force for the rest of bodies
+	for (int i = 1; i < nbodies; i++){
+		Spring sp(bodies[i - 1].posLocalToGlobal(-axis), bodies[i].posLocalToGlobal(axis), k);
+		F[i - 1] += sp.getF1();
+		F[i] += sp.getF2();
+		T[i - 1] += Vector3::crossProd(bodies[i - 1].vecLocalToGlobal(-axis), sp.getF1());
+		T[i] += Vector3::crossProd(bodies[i].vecLocalToGlobal(axis), sp.getF2());
+	}
+
+}
+
+
 void AdvanceTimeStep(vector<RigidBody>& bodies, float step, bool constraint, bool collisions, float floor)
 {
+	(constraint) ? advanceConstraints(bodies, step, collisions, floor) : advanceSprings(bodies, step, collisions, floor);
 }
 
 // Linear system example
